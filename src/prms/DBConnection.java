970fbfc4b46da559312474a7114dce84fa1a826d
Jhon Brix Brion <jbb7102@gmail.com>
2022-01-23 21:09:39 +0800

    Fixed Database Related Error.
    Updated an Error from previous commit.
    Fixed a code at DBConnection.java
    for JDBC/ConnectorJ Connection from Program.
    to MySQL Database (WAMP).

--- src/prms/DBConnection.java
+++ src/prms/DBConnection.java
@@ -3,24 +3,33 @@ package prms;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBConnection {
 
-    public static Connection connectDB() throws ClassNotFoundException {
+    public static Connection connectDB() {
 
-        
-        Connection conn = null;
         try {
             
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/prms", "root", "");
+            
+        } catch (ClassNotFoundException ex) {
+            
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        Connection conn = null;
+        try {
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/prms", "root", "");
+
             return conn;
 
         } catch (SQLException ex) {
 
             System.out.println("The system was unable to connect to the database!");
             return null;
-
         }
     }
 }
